b1d85193e9b348968b51724d3c0b7e182476484e
Talal Abbas <talalabbas84@gmail.com>
2022-12-16 21:01:19 +0500

    remove the private configs

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,28 +1,28 @@
 import { ChainIDs, Contracts, IContractAddress } from '@config/types';
 
 const vault: IContractAddress = {
-	[ChainIDs.ETHEREUM]: '0xE71074e1F97a1188692765ff4013bA5d9D7EF310',
-	[ChainIDs.RINKEBY]: '0xE71074e1F97a1188692765ff4013bA5d9D7EF310',
+  [ChainIDs.ETHEREUM]: '',
+  [ChainIDs.RINKEBY]: ''
 };
 
 const diceGame: IContractAddress = {
-	[ChainIDs.ETHEREUM]: '0x19B4c26C3c3C000b0dFc2Ee681Fb96bd371a3AEF',
-	[ChainIDs.RINKEBY]: '0x19B4c26C3c3C000b0dFc2Ee681Fb96bd371a3AEF',
+  [ChainIDs.ETHEREUM]: '',
+  [ChainIDs.RINKEBY]: ''
 };
 
 const chessGame: IContractAddress = {
-	[ChainIDs.ETHEREUM]: '0x5903eecECb220a3b4CeC2939022f0191b4F1575E',
-	[ChainIDs.RINKEBY]: '0x5903eecECb220a3b4CeC2939022f0191b4F1575E',
+  [ChainIDs.ETHEREUM]: '',
+  [ChainIDs.RINKEBY]: ''
 };
 
 export const addresses = {
-	vault,
-	diceGame,
-	chessGame,
+  vault,
+  diceGame,
+  chessGame
 } as const;
 
 export const ALL_CONTRACTS = Object.freeze(
-	Object.keys(addresses)
+  Object.keys(addresses)
 ) as Contracts[];
 
 export default addresses;
